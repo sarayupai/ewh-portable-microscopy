fe58685bb160616a05f0e305539e0ec1da2922b4
Sarayu Pai <sarujpai@gmail.com>
2021-05-22 16:10:07 -0700

    updated model info and api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://sarayupai.github.io/ewh-portable-microscopy/api',
+  API_ENDPOINT: 'https://sarayupai.github.io/ewh-portable-microscopy/',
   //API_ENDPOINT: 'http://localhost:5000/api'
 };
